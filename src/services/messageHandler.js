cbd5299f79f93ba007194f3775eac6d8a371166a
rialcard <racardenasgarcia@gmail.com>
2025-04-02 09:17:11 -0500

    cambio a paradigma mvc y se actualiza mensaje bienvenida

---
+++ src/services/messageHandler.js
@@ -0,0 +1,31 @@
+import whatsappService from './whatsappService.js';
+
+class MessageHandler {
+  async handleIncomingMessage(message) {
+    if (message?.type === 'text') {
+      const incomingMessage = message.text.body.toLowerCase().trim();
+
+      if(this.isGreeting(incomingMessage)){
+        await this.sendWelcomeMessage(message.from, message.id)
+      } else {
+        const response = `Echo: ${message.text.body}`;
+        await whatsappService.sendMessage(message.from, response, message.id);
+      }
+      await whatsappService.markAsRead(message.id);
+    }
+  }
+
+  isGreeting(message) {
+    const greetings = ["hola", "hello", "hi", "buenas tardes"];
+    return greetings.includes(message);
+  }
+
+  async sendWelcomeMessage(to, messageId) {
+    const welcomeMessage = "Hola, Bienvenido a nuestro servicio de veterinaria online." + 
+    "¿En Qué puedo ayudarte Hoy?";
+    await whatsappService.sendMessage(to, welcomeMessage, messageId);
+  }
+
+}
+
+export default new MessageHandler();
